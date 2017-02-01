25f0cb8bdeef2135007acf422330afd002d5c70d
ksindorf000 <ksindorf000@gmail.com>
2017-02-01 15:35:44 -0500

    Added ^ calculation

--- WebFormCalc/Default.aspx.cs
+++ WebFormCalc/Default.aspx.cs
@@ -8,12 +8,14 @@ using System.Web.UI.WebControls;
 public partial class _Default : System.Web.UI.Page
 {
     //Class level attribute - accessible to markup files
-    public List<string> Options = new List<string>() { "+", "-", "*", "/" };
+    public List<string> Options = new List<string>() { "+", "-", "*", "/", "^" };
     public string leftNum;
     public string operation;
     public string rightNum;
     public float results;
     public string rString;
+    public float lNum;
+    public float rNum;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -25,20 +27,27 @@ public partial class _Default : System.Web.UI.Page
             operation = Request.Form["operation"];
             rightNum = Request.Form["rightNum"];
 
+            lNum = float.Parse(leftNum);
+            rNum = float.Parse(rightNum);
+
             switch (operation)
             {
                 case "+":
-                    results = float.Parse(leftNum) + float.Parse(rightNum);
+                    results = lNum + rNum;
                     break;
                 case "-":
-                    results = float.Parse(leftNum) - float.Parse(rightNum);
+                    results = lNum - rNum;
                     break;
                 case "*":
-                    results = float.Parse(leftNum) * float.Parse(rightNum);
+                    results = lNum * rNum;
                     break;
                 case "/":
                     //Division();
-                    results = float.Parse(leftNum) / float.Parse(rightNum);
+                    results = lNum / rNum;
+                    break;
+                case "^":
+                    /* http://stackoverflow.com/questions/4584793/math-pow-gives-cannot-implicitly-convert-type-double-to-float-error */
+                    results = (float)Math.Pow(lNum, rNum);
                     break;
                 default:
                     results = 0;
